d312259543f9a45159fcf9c74b73b17c82bc8e1f
Virteip <sergiopietri@gmail.com>
2020-01-31 19:30:59 +0000

    [bubble sort, missing commas (,)]

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,8 +6,28 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+  int sorted = 0;
+  size_t i, j;
+  int temp;
+  
+  while(sorted == 0)
+    {
+      sorted = 1;
+      for (i = 0; i< size-1; i++)
+	{
+	  if(array[i] > array[i + 1])
+	    {
 
-
-
-
+	      temp = array[i]; 
+	      array[i] = array[i + 1]; 
+	      array[i + 1] = temp; 
+	      sorted = 0;
+	      for(j  = 0; j <= size-1; j++)
+		{
+		  printf("%d ", array[j]);
+		}
+	      putchar('\n');
+	    }
+	}
+    }
 }
