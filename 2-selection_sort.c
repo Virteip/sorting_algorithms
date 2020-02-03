e702d748a84e871cefbb71617475b963a54972a2
Virteip <sergiopietri@gmail.com>
2020-02-03 15:45:33 +0000

    [task #2 betty]

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,21 +7,21 @@
  */
 void selection_sort(int *array, size_t size)
 {
-  size_t i, j, temp, hold; 
-  
-  for (i = 0; i < size-1; i++) 
-    { 
-      temp = i; 
-      for (j = i+1; j < size; j++) 
-	{
-	  if (array[j] < array[temp])
-	    { 
-	      min_idx = j; 
-	    }
-	}
-      hold = *(&array[temp]); 
-      *(&array[temp]) = *(&array[i]); 
-      *(&array[i]) = hold;
-      print_array(array, size);
-    } 
+size_t i, j, temp, hold;
+
+for (i = 0; i < size - 1; i++)
+{
+temp = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[temp])
+{
+temp = j;
+}
+}
+hold = array[temp];
+array[temp] = array[i];
+array[i] = hold;
+print_array(array, size);
+}
 }
