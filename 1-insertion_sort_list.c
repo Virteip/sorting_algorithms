e10365ff426c69862da0be2119b9d9424eb6ec9f
Emmanuel Antoniogo mez Martinez <shinji.ogami@gmail.com>
2020-02-03 14:46:08 -0500

    Add files via upload

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /*
- * insertion_sort_list - sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm
+ * insertion_sort_list - Sort algorithm
  * @list: double pointer
  *
  * Return: None
@@ -9,40 +9,37 @@
 
 void insertion_sort_list(listint_t **list)
 {
-  listint_t *present, *owner, *node_owner;
-  int tmp;
+listint_t *present, *owner, *node_owner;
+int tmp;
 
-  if (list == NULL || *list == NULL || (*list)->next == NULL)
-    return;
-  present = (*list)->next;
-  while (present != NULL)
-    {
-      owner = present;
-      while (owner->prev != NULL)
-        {
-          tmp = 0;
-          if (owner->n < owner->prev->n)
-            {
-              if (owner->prev->prev != NULL)
-                owner->prev->prev->next = owner;
-              owner->prev->next = owner->next;
-              node_owner = owner->prev->prev;
-              owner->prev->prev = owner;
-              if (owner->next != NULL)
-                owner->next->prev = owner->prev;
-              owner->next = owner->prev;
-              owner->prev = node_owner;
-              tmp = 1;
-              if (owner->prev == NULL)
-                *list = owner;
-              print_list(*list);
-
-            }
-          if (tmp == 0)
-            owner= owner->prev;
-
-        }
-      present = present->next;
-
-    }
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+return;
+present = (*list)->next;
+while (present != NULL)
+{
+owner = present;
+while (owner->prev != NULL)
+{
+tmp = 0;
+if (owner->n < owner->prev->n)
+{
+if (owner->prev->prev != NULL)
+owner->prev->prev->next = owner;
+owner->prev->next = owner->next;
+node_owner = owner->prev->prev;
+owner->prev->prev = owner;
+if (owner->next != NULL)
+owner->next->prev = owner->prev;
+owner->next = owner->prev;
+owner->prev = node_owner;
+tmp = 1;
+if (owner->prev == NULL)
+*list = owner;
+print_list(*list);
+}
+if (tmp == 0)
+owner = owner->prev;
+}
+present = present->next;
+}
 }
